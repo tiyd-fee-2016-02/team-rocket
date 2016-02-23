642a7acfbb9ddf285058022924dcbef1093a90e3
Byron Wall <byrondevonwall@gmail.com>
2016-02-23 14:07:32 -0500

    Add js functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,20 +4,27 @@ $(document).ready(function(){
     $(".tab").removeClass("clicked");
     $( this ).addClass( "clicked" );
     $(".contributions-block").show();
+    $(".public-block").hide();
+    $( ".repo-block" ).hide();
   });
   $(".repos").click(function(){
     $(".tab").removeClass("clicked");
     $( this ).addClass( "clicked" );
-    $(".contributions-block").show();
+    $( ".repo-block" ).show();
+    $( ".contributions-block" ).hide();
+    $(".public-block").hide();
   });
   $(".public").click(function(){
     $(".tab").removeClass("clicked");
     $( this ).addClass( "clicked" );
-    $(".contributions-block").show();
+    $(".public-block").show();
+    $( ".contributions-block" ).hide();
+    $( ".repo-block" ).hide();
   });
 
 
 });
 
-  $( ".contributions-block" ).hide();
-  $( ".repo-block" ).hide();
+  // $( ".contributions-block" ).hide();
+  // $( ".repo-block" ).hide();
+  // $(".public-block").hide();
