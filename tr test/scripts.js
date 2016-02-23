2fdffcce782cf4addf7df0fd04e15a17e556c22f
alexanderallis <alexandreallis@gmail.com>
2016-02-22 22:37:52 -0500

    added a test file to sort things out. Which was successful but is still not quite complete.

---
+++ tr test/scripts.js
@@ -0,0 +1,6 @@
+// $( ".contributions" ).click(function() {
+//   $(".tab").removeClass("clicked");
+//   $( this ).addClass( "clicked" );
+// });
+// console.log("hello");
+$(".contributions-block").addClass(".display");
