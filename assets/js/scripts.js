13d208ce4899a9f6714fef4af6c1388fa1762196
Byron Wall <byrondevonwall@gmail.com>
2016-02-24 11:26:59 -0500

    add more js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -78,9 +78,21 @@ $(document).ready(function(){
 
    $.getJSON("https://api.github.com/users/octocat/repos", function(json){
       $(".repo-name-1").html(json[4].name);
-      console.log(json[4].name);
-      // $(".repo-content-1").html(json[4].content);
-      console.log(json[4].description);
+      $(".repo-content-1").html(json[4].description);
+      $(".num-stars-1").html(json[4].stargazers_count);
+      $(".repo-name-2").html(json[3].name);
+      $(".repo-content-2").html(json[3].description);
+      $(".num-stars-2").html(json[3].stargazers_count);
+      $("repo-name-3").html(json[2].name);
+      $(".repo-content-3").html(json[2].description);
+      $(".num-stars-3").html(json[2].stargazers_count);
+      $("repo-name-4").html(json[1].name);
+      $(".repo-content-4").html(json[1].description);
+      $(".num-stars-4").html(json[1].stargazers_count);
+      $("repo-name-5").html(json[0].name);
+      $(".repo-content-5").html(json[0].description);
+      $(".num-stars-5").html(json[0].stargazers_count);
+
    })
 
 });
