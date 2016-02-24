46619d21b6637a46ea845ebef2cbe849e73a2873
Byron Wall <byrondevonwall@gmail.com>
2016-02-24 12:13:59 -0500

    basically finish mvp

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -101,12 +101,6 @@ $(document).ready(function(){
 
 
       //for repos tab
-<<<<<<< HEAD
-      $(".num-forked-1").html(json[0].forks_count);
-      $(".rep-name-1").html(json[0].name);
-      $(".repo-desc-1").html(json[0].description);
-      $(".num-stars-1").html(json[0].stargazers_count);
-=======
       $(".num-forked-1").html(json[4].forks_count);
       $(".rep-name-1").html(json[4].name);
       $(".repo-desc-1").html(json[4].description);
@@ -121,7 +115,16 @@ $(document).ready(function(){
       $(".rep-name-3").html(json[2].name);
       $(".repo-desc-3").html(json[2].description);
       $(".num-stars-r-3").html(json[2].stargazers_count);
->>>>>>> bca82d86844e986760fb7bad8a4a375e71f11c71
+
+      $(".num-forked-4").html(json[1].forks_count);
+      $(".rep-name-4").html(json[1].name);
+      $(".repo-desc-4").html(json[1].description);
+      $(".num-stars-r-4").html(json[1].stargazers_count);
+
+      $(".num-forked-5").html(json[0].forks_count);
+      $(".rep-name-5").html(json[0].name);
+      $(".repo-desc-5").html(json[0].description);
+      $(".num-stars-r-5").html(json[0].stargazers_count);
    })
 
 });
