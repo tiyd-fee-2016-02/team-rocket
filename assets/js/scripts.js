875359174cecf8d5f1bb2e4c310c3394d0242f4d
Byron Wall <byrondevonwall@gmail.com>
2016-02-25 07:59:07 -0500

    clean up

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -47,73 +47,13 @@ $(document).ready(function(){
       $(".link").html(json.html_url);
       $(".joined").html(time);
       $(".follows").html(json.followers);
-      // $(".starrrrred").html(json.starred_url);
       $(".followingg").html(json.following);
-      //
-      // var img = _.template('<img src="<%- m.avatar_url %>" alt="avatar"></img>', {variable: 'm'});
-      // var tempImg = img({avatar_url: json.avatar_url});
-      // $(".usr-img").html(tempImg);
-      //
-      //
-      // var name = _.template('<%- m.name %>', { variable: 'm' });
-      // var tempName = name({ name: json.name});
-      // $('.my-name').html(tempName);
-      //
-      // var userName = _.template('<%- m.login %>', {variable: 'm'});
-      // var tempUserName = userName({login: json.login});
-      // $('.user-name').html(tempUserName);
-      //
-      // var org = _.template('<%- m.company %>', {variable: 'm'});
-      // var tempOrg = org({company: json.company});
-      // $(".organization").html(tempOrg);
-      //
-      // var loc = _.template('<%- m.location %>', {variable: 'm'});
-      // var tempLoc = loc({location: json.location});
-      // $(".location").html(tempLoc);
-      //
-      // var email = _.template('<%- m.email %>', {variable: 'm'});
-      // var tempEmail = email({email: json.email});
-      // $(".email").html(tempEmail);
-      //
-      // var link = _.template('<%- m.html_url %>', {variable: 'm'});
-      // var tempLink = link({html_url: json.html_url});
-      // $(".link").html(tempLink);
-      //
-      // var followers = _.template('<%- m.followers %>', {variable: 'm'});
-      // var tempFollowers = followers({followers: json.followers});
-      // $(".follows").html(json.following);
-      //
-      // var following = _.template('<%- m.following %>', {variable: 'm'});
-      // var tempFollowing = following({following: json.following});
-      // $(".followingg").html(tempFollowing);
    });
 
    $.getJSON("https://api.github.com/users/octocat/repos", function(json){
-     console.log(_.size(json));
-      ///for 'popular repos' tab
-      // $(".repo-name-1").html(json[4].name);
-      // $(".repo-content-1").html(json[4].description);
-      // $(".num-stars-1").html(json[4].stargazers_count);
-      //
-      // $(".repo-name-2").html(json[2].name);
-      // $(".repo-content-2").html(json[2].description);
-      // $(".num-stars-2").html(json[2].stargazers_count);
-      //
-      // $(".repo-name-3").html(json[1].name);
-      // $(".repo-content-3").html(json[1].description);
-      // $(".num-stars-3").html(json[1].stargazers_count);
-      //
-      // $(".repo-name-4").html(json[3].name);
-      // $(".repo-content-4").html(json[3].description);
-      // $(".num-stars-4").html(json[3].stargazers_count);
-      //
-      // $(".repo-name-5").html(json[0].name);
-      // $(".repo-content-5").html(json[0].description);
-      // $(".num-stars-5").html(json[0].stargazers_count);
-
+   //this code via tori from http://www.levihackwith.com/code-snippet-how-to-sort-an-array-of-json-objects-by-property/
       var counter = _.size(json);
       function sortByProperty(property) {
-        'use strict';
         return function (a, b) {
          var sortStatus = 0;
          if (a[property] < b[property]) {
@@ -128,63 +68,21 @@ $(document).ready(function(){
      json.sort(sortByProperty('stargazers_count'));
      if(counter-1 <= 5){
        for(var i = counter-1; i>=0; i--){
-         console.log(i);
          $(".contrib-block").append('<div class="repository"><span class="octicon octicon-repo"></span><span class="repo-text"><a class="repo-name-1" href="#">'+json[i].name+'</a><p class="repo-content-1">'+json[i].description+'</p></span><div class="stars"><div class="num-stars num-stars-1">'+json[i].stargazers_count+'</div><div class="octicon octicon-star"></div></div></div>');
      }
    }
      else if(counter-1 > 5){
        for(var i = counter-1; i>=counter-5; i--){
-         console.log(i);
          $(".contrib-block").append('<div class="repository"><span class="octicon octicon-repo"></span><span class="repo-text"><a class="repo-name-1" href="#">'+json[i].name+'</a><p class="repo-content-1">'+json[i].description+'</p></span><div class="stars"><div class="num-stars num-stars-1">'+json[i].stargazers_count+'</div><div class="octicon octicon-star"></div></div></div>');
      }
     }
-
-
-
-      //for repos tab
-      // var timeUd1 = new Date(json[4].updated_at).toLocaleString();
-      // $(".num-forked-1").html(json[4].forks_count);
-      // $(".rep-name-1").html(json[4].name);
-      // $(".repo-desc-1").html(json[4].description);
-      // $(".num-stars-r-1").html(json[4].stargazers_count);
-      // $(".repo-time-ud-1").html("Updated: "+timeUd1);
-      //
-      // var timeUd2 = new Date(json[3].updated_at).toLocaleString();
-      // $(".num-forked-2").html(json[3].forks_count);
-      // $(".rep-name-2").html(json[3].name);
-      // $(".repo-desc-2").html(json[3].description);
-      // $(".num-stars-r-2").html(json[3].stargazers_count);
-      // $(".repo-time-ud-2").html("Updated: "+timeUd2);
-      //
-      // var timeUd3 = new Date(json[2].updated_at).toLocaleString();
-      // $(".num-forked-3").html(json[2].forks_count);
-      // $(".rep-name-3").html(json[2].name);
-      // $(".repo-desc-3").html(json[2].description);
-      // $(".num-stars-r-3").html(json[2].stargazers_count);
-      // $(".repo-time-ud-3").html("Updated: "+timeUd3);
-      //
-      //
-      // var timeUd4 = new Date(json[1].updated_at).toLocaleString();
-      // $(".num-forked-4").html(json[1].forks_count);
-      // $(".rep-name-4").html(json[1].name);
-      // $(".repo-desc-4").html(json[1].description);
-      // $(".num-stars-r-4").html(json[1].stargazers_count);
-      // $(".repo-time-ud-4").html("Updated: "+timeUd4);
-      //
-      // var timeUd5 = new Date(json[0].updated_at).toLocaleString();
-      // $(".num-forked-5").html(json[0].forks_count);
-      // $(".rep-name-5").html(json[0].name);
-      // $(".repo-desc-5").html(json[0].description);
-      // $(".num-stars-r-5").html(json[0].stargazers_count);
-      // $(".repo-time-ud-5").html("Updated: "+timeUd5);
    });
 
 
-   $.getJSON("https://api.github.com/users/octocat/repos", function(json){
+   $.getJSON("https://api.github.com/users/byrondevonwall/repos", function(json){
      var counter = _.size(json);
      //this code via tori from http://www.levihackwith.com/code-snippet-how-to-sort-an-array-of-json-objects-by-property/
      function sortByProperty(property) {
-       'use strict';
        return function (a, b) {
         var sortStatus = 0;
         if (a[property] < b[property]) {
